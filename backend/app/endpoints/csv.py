b3e9e0deee72bca7c910109a1bea5a47dc180f7f
GitHermann <hermann.huang@efrei.net>
2024-02-28 15:47:50 +0100

    Added post /csv endpoint

--- backend/app/endpoints/csv.py
+++ backend/app/endpoints/csv.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile
 import os
 
 data_path = 'data'
@@ -15,3 +15,14 @@ async def get_all_datasets():
 def list_csv_files(path : str):
   csv_files = [file for file in os.listdir(path) if file.endswith('.csv')]
   return csv_files
+
+@router.post('/csv')
+async def post_csv(csv_file: UploadFile):
+  '''
+    Saves a copy of the uploaded csv file into the file system
+  '''
+  file_path = os.path.join(data_path, csv_file.filename)
+
+  with open(file_path, 'wb+') as new_file:
+    new_file.write(csv_file.file.read())
+  return {"message": f"File '{csv_file.filename}' saved at '{file_path}'"}
