12f559d310c95195310b8434b1a0c9aa1165b2bd
GitHermann <hermann.huang@efrei.net>
2024-02-29 09:20:34 +0100

    Refactored test file size

--- backend/test/test_main.py
+++ backend/test/test_main.py
@@ -8,6 +8,7 @@ client = TestClient(app)
 
 data_path = '../data'
 test_filename = 'test_csv_file.csv'
+size = 0
 
 def test_hello_world():
   response = client.get("/")
@@ -24,6 +25,8 @@ def test_post_csv():
     test_csv_file.write("invoice id, email, country, invoicing date, amount")
 
   with open(test_filename, "rb") as test_csv_file:
+    global size
+    size = os.path.getsize(test_filename)
     files = {"csv_file": test_csv_file}
     response = requests.post('http://127.0.0.1:8000/csv', files=files)
   
@@ -35,4 +38,4 @@ def test_post_csv():
 def test_get_dataset():
   response = client.get('/csv/test_csv_file')
   assert response.status_code == 200
-  assert response.json() == {"file": test_filename, "size": 50}
+  assert response.json() == {"file": test_filename, "size": size}
