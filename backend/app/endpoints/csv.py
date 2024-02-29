82eabca8dba927c36506f24978c394b76364dc97
GitHermann <hermann.huang@efrei.net>
2024-02-29 11:53:52 +0100

    refactored find_file_if_exists

--- backend/app/endpoints/csv.py
+++ backend/app/endpoints/csv.py
@@ -27,17 +27,24 @@ async def post_csv(csv_file: UploadFile):
     new_file.write(csv_file.file.read())
   return {"id": csv_file.filename}
 
+def find_file_if_exists(filename: str):
+  for file in list_csv_files(data_path):
+    if file == filename:
+      return file
+  return
+
 @router.get('/csv/{id}', status_code=404)
 async def get_dataset(id: str, response: Response):
   '''
     Returns the file name and the size of the dataset
   '''
   filename = id + '.csv'
-  for file in list_csv_files(data_path):
-    if file == filename:
-      response.status_code = status.HTTP_200_OK
-      return {"file": file,  "size": os.path.getsize(os.path.join(data_path,filename))}
-  return
+  file = find_file_if_exists(filename)
+  if file is not None:
+    response.status_code = status.HTTP_200_OK
+    return {"file": file,  "size": os.path.getsize(os.path.join(data_path,filename))}
+  else:
+    return
 
 @router.delete('/csv/{id}', status_code=404)
 async def delete_dataset(id: str, response: Response):
@@ -45,9 +52,22 @@ async def delete_dataset(id: str, response: Response):
     Deletes the dataset
   '''
   filename = id + '.csv'
-  for file in list_csv_files(data_path):
-    if file == filename:
-      os.remove(os.path.join(data_path,filename))
-      response.status_code == status.HTTP_200_OK
-      return {"message": "File removed"}
-  return
+  file = find_file_if_exists(filename)
+  if file is not None:
+    os.remove(os.path.join(data_path,filename))
+    response.status_code == status.HTTP_200_OK
+    return {"message": "File removed"}
+  else:
+    return
+
+@router.get('/csv/{id}/plot')
+async def get_amount_per_customer_per_month(id: str):
+  '''
+    Returns a formatted dataset containing the amount paid by each customer per month
+  '''
+  filename = id + '.csv'
+  file = find_file_if_exists(filename)
+  if file is not None:
+    return
+  else:
+    return
