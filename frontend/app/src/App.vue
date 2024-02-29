603f6b6cb375f70d8247e477534cf68dcc53cefa
GitHermann <hermann.huang@efrei.net>
2024-02-29 23:58:17 +0100

    Frontend app initial commit

---
+++ frontend/app/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="main">
+    <div class="leftBox">
+      <div class="test">
+        <APIButton name="List the datasets" />
+      </div>
+      <div class="test"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import APIButton from "./components/APIButton.vue";
+
+export default {
+  components: {
+    APIButton,
+  },
+  methods: {
+    async test() {
+      console.log("HELLO");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.main {
+  height: 100vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: rgb(245, 245, 245);
+}
+
+.leftBox {
+  width: 40%;
+  height: 85vh;
+  margin: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+}
+
+.test {
+  width: 100%;
+  height: 45%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  border-radius: 30px;
+  box-shadow: 3px 3px 3px rgb(230, 230, 230);
+  text-align: center;
+}
+</style>
+
+<style>
+* {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ frontend/app/src/components/APIButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="APIButton">
+    <span>{{ name }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ["name"],
+};
+</script>
+
+<style scoped>
+.APIButton {
+  width: 20%;
+  padding: 10px;
+  border: none;
+  border-radius: 30px;
+  background-color: rgb(0, 100, 255);
+  font-weight: bold;
+  color: white;
+}
+.APIButton:hover {
+  filter: brightness(1.25);
+  transition: 0.25s;
+}
+.APIButton:active {
+  filter: brightness(0.75);
+  transform: scale(0.95);
+  transition: 0.25s;
+}
+</style>
